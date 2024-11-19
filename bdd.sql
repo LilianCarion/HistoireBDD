d9c857e12d9eb4497c9701bbc2958c88ce163994
LilianCarion <91951060+LilianCarion@users.noreply.github.com>
2024-11-19 11:32:44 +0100

    push

--- bdd.sql
+++ bdd.sql
@@ -7,7 +7,6 @@ Create table if not exists choices (
     id         int auto_increment primary key,
     message_id int,
     content    text,
-    orderM      tinyint,
     message_id_next int
 );
 Create table if not exists path(
@@ -17,3 +16,14 @@ Create table if not exists path(
 
 alter table choices add constraint fk_message_id foreign key (message_id) references messages(id);
 alter table choices add constraint fk_message_id_next foreign key (message_id_next) references messages(id);
+
+create trigger after_update_path_trigger
+    before update on path
+    for each row
+begin
+    begin
+        if new.path not like '%;%' and new.path != '1' then
+            set new.path = concat(old.path, ';', new.path);
+        end if;
+    end;
+end;
--- src/Controller/StoryController.php
+++ src/Controller/StoryController.php
@@ -43,12 +43,48 @@ class StoryController extends AbstractController
 
     public function getChoices(int $id): array | Response
     {
-        $sql = 'SELECT content, orderM FROM choices WHERE message_id = :id';
+        $sql = 'SELECT content, id_message_next FROM choices WHERE message_id = :id';
         $stmt = $this->connection->executeQuery($sql, ['id' => $id]);
         $result = $stmt->fetchAllAssociative();
         if (!$result) {
             return new Response('Message non trouvé', 404);
         }
         return $result;
+    }
+
+    #[Route('/story/{id}')]
+    public function displayMessage(int $id)
+    {
+        $this->updatePath($id);
+        $this->message = $this->getMessage($id);
+        $this->choices = $this->getChoices($id);
+
+        return $this->render('story/index.html.twig', [
+            'message' => $this->message,
+            'choices' => $this->choices
+        ]);
+    }
+
+    public function updatePath(int $id)
+    {
+        $sql = 'UPDATE path set path = :id WHERE id = 1';
+        $this->connection->executeQuery($sql, ['id' => $id]);
+    }
+    #[Route('/back')]
+    public function back() {
+        $sql = "UPDATE path 
+                SET path = SUBSTRING_INDEX(path, ';', GREATEST(1, LENGTH(path) - LENGTH(REPLACE(path, ';', ''))))
+                WHERE id = 1;";
+        $this->connection->executeQuery($sql);
+
+        $sql = "SELECT SUBSTRING_INDEX(path, ';', -1) as last_segment FROM path WHERE id = 1";
+        $lastSegment = $this->connection->fetchOne($sql);
+        $this->message = $this->getMessage($lastSegment);
+        $this->choices = $this->getChoices($lastSegment);
+
+        return $this->render('story/index.html.twig', [
+            'message' => $this->message,
+            'choices' => $this->choices
+        ]);
     }
 }
